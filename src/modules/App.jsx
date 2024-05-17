4deeb622fa3ea68ae504ae4528a307508e5adb24
toomach <yaroshpavlo@gmail.com>
2024-05-17 22:09:06 +0300

    starts working

---
+++ src/modules/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() => {
+
+};                   // function  - це компонент, компонент завжди пишеться з великої літери 
+ 
+
+export default App
